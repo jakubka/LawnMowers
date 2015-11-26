fb9a0e7a650dd73a6780fe1392716cd3319836b3
Jakub Kadlubiec <Jakub.Kadlubiec@imaginecommunications.com>
2015-11-26 01:09:54 +0000

    Add Position class and tests

---
+++ src/LawnMowers.Domain/Model/Position.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LawnMowers.Domain.Model
+{
+    public struct Position
+    {
+        public Position(
+            int x,
+            int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; }
+
+        public int Y { get; }
+
+        public Position GetAdjacentPosition(
+            Direction direction)
+        {
+            if (direction == null)
+            {
+                throw new ArgumentNullException(nameof(direction));
+            }
+
+            return new Position(X + direction.DeltaX, Y + direction.DeltaY);
+        }
+    }
+}
---
+++ test/LawnMowers.Domain.Tests/Model/PositionTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using FluentAssertions;
+using LawnMowers.Domain.Model;
+using Xunit;
+
+namespace LawnMowers.Domain.Tests.Model
+{
+    public class PositionTests
+    {
+        [Fact]
+        public void GetAdjacentPosition_ShouldReturnCorrectPosition()
+        {
+            var position = new Position(2, 3);
+
+            var adjacentPosition =
+                position.GetAdjacentPosition(Direction.North);
+
+            adjacentPosition.X.Should().Be(2);
+            adjacentPosition.Y.Should().Be(4);
+        }
+
+        [Fact]
+        public void GetAdjacentPosition_ShouldThrowWhenNullIsPassedIn()
+        {
+            var position = new Position(2, 3);
+
+            Action call = () => position.GetAdjacentPosition(null);
+            call.ShouldThrow<ArgumentNullException>();
+        }
+    }
+}
