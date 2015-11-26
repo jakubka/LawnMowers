e440df36d9571561b2e17d19fed50563232e0926
Jakub Kadlubiec <Jakub.Kadlubiec@imaginecommunications.com>
2015-11-26 01:11:30 +0000

    Add LawnMowerState class and tests

---
+++ src/LawnMowers.Domain/Model/LawnMowerState.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace LawnMowers.Domain.Model
+{
+    public class LawnMowerState
+    {
+        public LawnMowerState(
+            Position position,
+            Direction direction)
+        {
+            if (direction == null)
+            {
+                throw new ArgumentNullException(nameof(direction));
+            }
+
+            Position = position;
+            Direction = direction;
+        }
+
+        public Position Position { get; }
+
+        public Direction Direction { get; }
+
+        public LawnMowerState GetStateAfterRightTurn()
+        {
+            return new LawnMowerState(
+                Position,
+                Direction.DirectionToTheRight);
+        }
+
+        public LawnMowerState GetStateAfterLeftTurn()
+        {
+            return new LawnMowerState(
+                Position,
+                Direction.DirectionToTheLeft);
+        }
+
+        public LawnMowerState GetStateAfterMoveForward()
+        {
+            return new LawnMowerState(
+                Position.GetAdjacentPosition(Direction),
+                Direction);
+        }
+
+        protected bool Equals(LawnMowerState other)
+        {
+            return Position.Equals(other.Position) && Equals(Direction, other.Direction);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((LawnMowerState) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (Position.GetHashCode()*397) ^ (Direction?.GetHashCode() ?? 0);
+            }
+        }
+    }
+}
---
+++ test/LawnMowers.Domain.Tests/Model/LawnMowerStateTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using FluentAssertions;
+using LawnMowers.Domain.Model;
+using Xunit;
+
+namespace LawnMowers.Domain.Tests.Model
+{
+    public class LawnMowerStateTests
+    {
+        [Fact]
+        public void Constructor_ShouldThrowWhenDirectionIsNull()
+        {
+            Action call = () => new LawnMowerState(new Position(2, 4), null);
+            call.ShouldThrow<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void GetStateAfterRightTurn_ShouldReturnCorrectNewState()
+        {
+            var originalState = new LawnMowerState(new Position(5, 10), Direction.East);
+
+            var expectedNewState = new LawnMowerState(new Position(5, 10), Direction.South);
+
+            var newState = originalState.GetStateAfterRightTurn();
+
+            newState
+                .Should()
+                .Be(expectedNewState);
+        }
+
+        [Fact]
+        public void GetStateAfterLeftTurn_ShouldReturnCorrectNewState()
+        {
+            var originalState = new LawnMowerState(new Position(5, 10), Direction.East);
+
+            var expectedNewState = new LawnMowerState(new Position(5, 10), Direction.North);
+
+            var newState = originalState.GetStateAfterLeftTurn();
+
+            newState
+                .Should()
+                .Be(expectedNewState);
+        }
+        
+        [Fact]
+        public void GetStateAfterMoveForward_ShouldReturnCorrectNewState()
+        {
+            var originalState = new LawnMowerState(new Position(5, 10), Direction.East);
+
+            var expectedNewState = new LawnMowerState(new Position(6, 10), Direction.East);
+
+            var newState = originalState.GetStateAfterMoveForward();
+
+            newState
+                .Should()
+                .Be(expectedNewState);
+        }
+    }
+}
