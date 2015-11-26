3f39405d3f45d1de6f4adb9f7ea73db1fcc8f766
Jakub Kadlubiec <Jakub.Kadlubiec@imaginecommunications.com>
2015-11-26 01:10:19 +0000

    Add LawnSize class and tests

---
+++ src/LawnMowers.Domain/Model/LawnSize.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace LawnMowers.Domain.Model
+{
+    public class LawnSize
+    {
+        public LawnSize(
+            int rightmostCoordinate,
+            int topmostCoordinate)
+        {
+            if (rightmostCoordinate < 0)
+            {
+                throw new ArgumentNullException(nameof(rightmostCoordinate), "Rightmost coordinate cannot be negative");
+            }
+            if (topmostCoordinate < 0)
+            {
+                throw new ArgumentNullException(nameof(topmostCoordinate), "Topmost coordinate cannot be negative");
+            }
+
+            RightmostCoordinate = rightmostCoordinate;
+            TopmostCoordinate = topmostCoordinate;
+        }
+
+        public int RightmostCoordinate { get; }
+
+        public int TopmostCoordinate { get; }
+
+        public bool IsPositionWithinLawn(
+            Position position)
+        {
+            return position.X >= 0
+                && position.X <= RightmostCoordinate
+                && position.Y >= 0
+                && position.Y <= TopmostCoordinate;
+        }
+
+        protected bool Equals(LawnSize other)
+        {
+            return RightmostCoordinate == other.RightmostCoordinate && TopmostCoordinate == other.TopmostCoordinate;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((LawnSize) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (RightmostCoordinate*397) ^ TopmostCoordinate;
+            }
+        }
+    }
+}
---
+++ test/LawnMowers.Domain.Tests/Model/LawnSizeTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using FluentAssertions;
+using LawnMowers.Domain.Model;
+using Xunit;
+
+namespace LawnMowers.Domain.Tests.Model
+{
+    public class LawnSizeTests
+    {
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(2, 2)]
+        [InlineData(5, 5)]
+        [InlineData(5, 10)]
+        public void IsPositionWithinLawn_ShouldReturnTrueWhenPositionIsWithin(
+            int positionX,
+            int positionY)
+        {
+            var lawnSize = new LawnSize(5, 10);
+            var position = new Position(positionX, positionY);
+
+            lawnSize
+                .IsPositionWithinLawn(position)
+                .Should()
+                .BeTrue();
+        }
+
+        [Theory]
+        [InlineData(-100, -100)]
+        [InlineData(-1, 2)]
+        [InlineData(6, 5)]
+        [InlineData(5, 11)]
+        [InlineData(6, 11)]
+        [InlineData(100, 100)]
+        public void IsPositionWithinLawn_ShouldReturnFalseWhenPositionIsNotWithin(
+            int positionX,
+            int positionY)
+        {
+            var lawnSize = new LawnSize(5, 10);
+            var position = new Position(positionX, positionY);
+
+            lawnSize
+                .IsPositionWithinLawn(position)
+                .Should()
+                .BeFalse();
+        }
+
+        [Theory]
+        [InlineData(-1, 10)]
+        [InlineData(10, -1)]
+        public void Constructor_ShouldThrowWhenCoordinateIsNegative(
+            int rightmostCoordinate,
+            int topmostCoordinate)
+        {
+            Action call = () => new LawnSize(rightmostCoordinate, topmostCoordinate);
+            call.ShouldThrow<ArgumentException>();
+        }
+
+        [Fact]
+        public void TwoTheSameLawnSizesShouldBeEqual()
+        {
+            var lawnSize1 = new LawnSize(5, 10);
+            var lawnSize2 = new LawnSize(5, 10);
+
+            (lawnSize1.Equals(lawnSize2))
+                .Should()
+                .BeTrue();
+        }
+
+        [Fact]
+        public void TwoDifferentLawnSizesShouldNotBeEqual()
+        {
+            var lawnSize1 = new LawnSize(5, 10);
+            var lawnSize2 = new LawnSize(10, 10);
+
+            (lawnSize1.Equals(lawnSize2))
+                .Should()
+                .BeFalse();
+        }
+    }
+}
